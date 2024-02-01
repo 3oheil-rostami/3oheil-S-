3ed68147c7e73d14b137a44ee3f9f713b9d01efb
PwZakarya <hasanzadahzakarya@gmail.com>
2024-02-02 00:56:14 +0330

    added new route for testing components 🧪

---
+++ src/app/test/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+import ContainerSlider from "@/components/ContainerSlider";
+import React from "react";
+
+export default function page() {
+	return <ContainerSlider type="promotion" />;
+}
