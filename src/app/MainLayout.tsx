18d3781b2cba36ffbf77d08af44ab5eaa7e15300
PwZakarya <hasanzadahzakarya@gmail.com>
2024-02-02 01:17:11 +0330

    added MainLayout.tsx 💉

---
+++ src/app/MainLayout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import RootLayout from "./layout";
+
+export default function MainLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<RootLayout>
+			<div>{children}</div>
+		</RootLayout>
+	);
+}
