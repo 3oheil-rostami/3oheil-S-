8b10b00805bd16aaeffba34dafdc6ee60cbd4ba9
PwZakarya <hasanzadahzakarya@gmail.com>
2024-02-02 00:53:42 +0330

    code Link.tsx Logic 📐

---
+++ src/components/Link.tsx
@@ -0,0 +1,18 @@
+import NextLink from "next/link";
+import React from "react";
+
+interface Props {
+	children?: string| any;
+	icon?: undefined | any;
+	href?: any;
+	className?: string;
+}
+
+export default function Link({ children, icon, href, className }: Props) {
+	return (
+		<NextLink href={href} className={`flex gap-1 items-center ${className}`}>
+			{icon && <span>{icon}</span>}
+			{children && <span>{children}</span>}
+		</NextLink>
+	);
+}
