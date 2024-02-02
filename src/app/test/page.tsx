3986285eee5bcf62fad7979561f18edc9ad42894
abolfazl <abolfazld.ir01@gmail.com>
2024-02-02 16:10:14 +0330

    remove extra code

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -3,5 +3,5 @@ import ContainerSlider from "@/components/ContainerSlider";
 import React from "react";
 
 export default function page() {
-	return <ContainerSlider type="promotion" />;
-}
+	return true;
+};
