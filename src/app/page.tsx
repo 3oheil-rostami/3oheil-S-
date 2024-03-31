65a35b2d1046428dd040c1791ee7baaeabc3ff20
PwZakarya <hasanzadahzakarya@gmail.com>
2024-04-01 00:13:32 +0330

    added more sections to Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,22 +4,29 @@ import Navbar from "@/components/Navbar";
 import NavbarMenu from "@/components/NavbarMenu";
 import HeaderSlider from "@/components/HeaderSlider";
 import ContainerSlider from "@/components/ContainerSlider";
+import CategoriesWithImages from "@/components/CategoriesWithImages";
+import Brands from "@/components/Brands";
+import Footer from "@/components/Footer";
+import LastArticles from "@/components/LastArticles";
 
 export default function Home() {
+	
 	return (
 		<MainLayout>
 			<Topbar />
-			<div className='w-full flex flex-col items-center justify-center bg-red-200'>
+			<div className='w-full flex flex-col items-center justify-center divide-y-2 divide-secondary-100'>
 				<Navbar />
 				<NavbarMenu />
 			</div>
 			<HeaderSlider />
 			<div className='w-full flex flex-col items-center justify-center gap-y-4 my-4'>
-				<ContainerSlider type='news-products' />
 				<ContainerSlider type='promotion' />
+				<CategoriesWithImages />
+				<Brands />
 				<ContainerSlider type='top-sales' />
-				<ContainerSlider type='news-products' />
+				<LastArticles />
 			</div>
+			<Footer />
 		</MainLayout>
 	);
 }
