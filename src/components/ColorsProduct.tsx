55324797a9e584600fc130ec59a2eca657929e51
PwZakarya <hasanzadahzakarya@gmail.com>
2024-04-12 17:07:56 +0330

    refactor logic ColorsProduct component

--- src/components/ColorsProduct.tsx
+++ src/components/ColorsProduct.tsx
@@ -3,12 +3,14 @@ import React from "react";
 
 export default function ColorsProduct({ colors }: ColorsProductsProps) {
 	return (
-		<div className='w-full flex flex-wrap items-center gap-2'>
+		<div className="w-full flex flex-wrap items-center gap-2">
 			{colors.map((color, index) => (
 				<span
-					className={`size-12 rounded-full ring-2 ring-secondary-900  `}
-					style={{ backgroundColor: color }}
-					key={index}></span>
+					key={index}
+					title={color.colorName}
+					style={{ backgroundColor: "" + color.colorCode }}
+					className={`size-12 rounded-full ring-2 ring-secondary-900`}
+				/>
 			))}
 		</div>
 	);
