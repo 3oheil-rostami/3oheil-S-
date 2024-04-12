ecdf5395940fe03cf5140db8e6d052742bf25925
PwZakarya <hasanzadahzakarya@gmail.com>
2024-04-12 17:12:08 +0330

    created NavDraw component for User dashboard

---
+++ src/components/user/NavDraw.tsx
@@ -0,0 +1,28 @@
+import { FaShoppingCart, FaUserEdit } from "react-icons/fa";
+import Avatar from "../Avatar";
+import IconButton from "../form/IconButton";
+import NavDrawLink from "../dashboard/NavDrawLink";
+import { SiCodefactor } from "react-icons/si";
+import { MdOutlineFavorite } from "react-icons/md";
+import Link from "next/link";
+
+export default function NavDraw() {
+	return (
+		<div className='h-full w-80 flex flex-col gap-3'>
+			<div className='border-2 rounded-xl p-6 flex justify-between items-center'>
+				<Link href={"/user"} className='flex items-center gap-2'>
+					<Avatar size='md' name='Za' />
+					<span className='text-neutral-600 text-xl font-bold'>09142960913</span>
+				</Link>
+				<IconButton colorScheme='primary' size='lg' variant='text'>
+					<FaUserEdit />
+				</IconButton>
+			</div>
+			<div className='border-2 rounded-xl p-6 flex flex-col gap-2'>
+				<NavDrawLink icon={<FaShoppingCart />} link='/user/cart' title='سبد خرید' />
+				<NavDrawLink icon={<SiCodefactor />} link='/user/orders' title='سفارشات' />
+				<NavDrawLink icon={<MdOutlineFavorite />} link='/user/favorites' title='علاقه مندی ها' />
+			</div>
+		</div>
+	);
+}
