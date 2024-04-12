409c7000551f2d45557178bda9ae9087797fcec4
PwZakarya <hasanzadahzakarya@gmail.com>
2024-04-12 17:23:57 +0330

    connected to api ⚡

--- src/app/categories/[categoryName]/page.tsx
+++ src/app/categories/[categoryName]/page.tsx
@@ -1,16 +1,33 @@
+"use server";
 import React from "react";
 import CategoryLayout from "./CategoryLayout";
 import ProductCart from "@/components/ProductCard";
+import { getCategory } from "@/services/category";
+import { CategoryPage } from "@/types/apiTypes";
 
-export default function page() {
+async function getData(href: string) {
+	try {
+		const response = await getCategory(href);
+		const data: CategoryPage = response.data;
+		return data;
+	} catch (error) {
+		console.error(":( error is  =>", error);
+		return undefined;
+	}
+}
+
+export default async function page({
+	params: { categoryName },
+}: {
+	params: { categoryName: string };
+}) {
+	const data: undefined | CategoryPage = await getData(categoryName);
+	// console.log("data:", data);
 	return (
-		<CategoryLayout>
-			<div className='w-full h-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-2 xl:grid-cols-3 gap-1 bg-neutral-100 '>
-				{Array(20)
-					.fill(9)
-					.map((_, index) => (
-						<ProductCart key={index} />
-					))}
+		<CategoryLayout breadcrumb={data?.address || []} categories={data?.categories || []}>
+			<div className="w-full h-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-2 xl:grid-cols-3 gap-1 bg-neutral-100 ">
+				{!!data &&
+					data.products.map(productItem => <ProductCart {...productItem} key={productItem._id} />)}
 			</div>
 		</CategoryLayout>
 	);
