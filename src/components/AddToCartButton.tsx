de9be4bb4df1d57e167d11d44a7f6b0acf90d7ac
PwZakarya <hasanzadahzakarya@gmail.com>
2024-04-12 17:11:27 +0330

    added white background color to main tag element and formatted

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -24,25 +24,25 @@ export default function AddToCartButton({
 
 	return (
 		<div
-			className={`w-fit h-9 min-w-9 border border-primary-600 rounded-full flex justify-center gap-2 items-center text-primary-700 p-px transition-all ${
+			className={`w-fit h-9 min-w-9 bg-white border border-primary-600 rounded-full flex justify-center gap-2 items-center text-primary-700 p-px transition-all ${
 				countNum === 0
 					? "*:hidden *:hover:hidden first:*:inline first:*:hover:inline"
 					: "*:hidden *:hover:inline first:*:inline first:*:hover:hidden"
 			}`}>
-			<button className='addToCard' onClick={incrementHandler}>
+			<button className="addToCard" onClick={incrementHandler}>
 				{countNum === 0 ? <MdOutlineAdd /> : countNum}
 			</button>
 			{countNum > 0 && (
 				<>
 					<button
-						className='increment p-1 text-base rounded-full bg-neutral-50 border-2 border-transparent hover:border-neutral-200 transition-all'
-						type='button'
+						className="increment p-1 text-base rounded-full bg-neutral-50 border-2 border-transparent hover:border-neutral-200 transition-all"
+						type="button"
 						onClick={incrementHandler}>
 						<MdOutlineAdd />
 					</button>
-					<span className='count font-bold text-lg'>{countNum}</span>
+					<span className="count font-bold text-lg">{countNum}</span>
 					<button
-						className='decrement p-1 text-base rounded-full bg-neutral-50 border-2 border-transparent hover:border-neutral-200 transition-all'
+						className="decrement p-1 text-base rounded-full bg-neutral-50 border-2 border-transparent hover:border-neutral-200 transition-all"
 						onClick={decrementHandler}>
 						<MdDeleteOutline />
 					</button>
