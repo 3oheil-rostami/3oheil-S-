53f2fb777e4474e4f1e69c011bd9a4b004d47819
PwZakarya <hasanzadahzakarya@gmail.com>
2024-04-12 17:06:55 +0330

    refactor logic and styles DetailsProduct component

--- src/components/DetailsProduct.tsx
+++ src/components/DetailsProduct.tsx
@@ -1,27 +1,47 @@
+import { Color } from "@/types/apiTypes";
 import ColorsProduct from "./ColorsProduct";
+import { FaRegStar, FaStar } from "react-icons/fa";
 
-export default function DetailsProduct() {
+export default function DetailsProduct({
+	properties,
+	colors,
+	score,
+}: {
+	properties: string[];
+	colors: Color[];
+	score: number;
+}) {
 	return (
-		<div className='w-full lg:w-1/2  border-t-2 border-neutral-300 pt-4 px-3  lg:order-none grow'>
+		<div className="w-full lg:w-1/2  border-t-2 border-neutral-300 pt-4 px-3 lg:order-none grow">
 			{/* <h2 className='text-lg font-bold text-neutral-700 mb-3 uppercase'>LONG MASCARA CITRAY</h2> */}
-			<div className='flex flex-wrap gap-x-4 gap-y-2 items-center mt-3'>
-				<span className='text-base font-normal text-neutral-800'>دسته بندی : </span>
-				<ul className='flex items-center gap-1 text-sm font-medium text-neutral-900'>
-					<li className=''>آرایشی ،</li>
-					<li className=''>آرایشی صورت،</li>
-					<li className=''>کرم صفید کننده</li>
+			<div className="flex gap-2 py-2">
+				<div className="flex gap-1">
+					<span className="text-sm text-neutral-800 font-bold">امتیازات:</span>
+					{Array(score)
+						.fill(score)
+						.map((_, index) => (
+							<FaStar key={index} className="text-yellow-600" />
+						))}
+					{Array(5 - score)
+						.fill(5 - score)
+						.map((_, index) => (
+							<FaRegStar key={index} className="text-neutral-700" />
+						))}
+				</div>
+				<span>|</span>
+				<span className="text-sm text-neutral-800 font-bold"> نظرات : 56 نظر</span>
+			</div>
+			<div>
+				<h2 className="text-lg font-bold text-neutral-700">ویژگی های محصول </h2>
+				<ul className="marker:text-primary-600 list-disc pr-7 pt-2 mb-4 max-h-48 overflow-hidden relative after:h-14 after:w-full after:absolute after:bottom-0 after:left-0 after:right-0 after:bg-gradient-to-t after:from-neutral-300 after:to-transparent after:hidden">
+					{properties.map((property, index) => (
+						<li key={index}>{property}</li>
+					))}
 				</ul>
 			</div>
-			<div className='mt-5'>
-				<h2 className='text-lg font-bold text-neutral-700'>ویژگی های محصول </h2>
-				<ul className='marker:text-primary-600 list-disc pr-7 pt-2 mb-4 max-h-48 overflow-hidden relative after:h-14 after:w-full after:absolute after:bottom-0 after:left-0 after:right-0 after:bg-gradient-to-t after:from-neutral-300 after:to-transparent after:hidden'>
-					<li className=''>ترکیبات گیاهی</li>
-					<li className=''>حجم دهنده</li>
-					<li className=''>حالت دهنده</li>
-				</ul>
-			</div>
-			<div className='mt-2'>
-				<ColorsProduct colors={["#840C22", "#510111", "#f97954"]} />
+			<div className="mt-2">
+				<h3 className="text-lg font-bold text-neutral-700 mb-3">رنگبندی محصول</h3>
+				<ColorsProduct colors={colors} />
 			</div>
 		</div>
 	);
