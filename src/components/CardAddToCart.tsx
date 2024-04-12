83548904c5b61a8aafcf4fc727df6b2cf0cbb6e8
PwZakarya <hasanzadahzakarya@gmail.com>
2024-04-12 17:10:15 +0330

    refactor logic and connected to api (CardAddToCart) component

--- src/components/CardAddToCart.tsx
+++ src/components/CardAddToCart.tsx
@@ -1,6 +1,4 @@
 import Image from "next/image";
-import React from "react";
-import brand from "@/../public/images/brand5.jpg";
 import Link from "./Link";
 import { CardAddToCartProps } from "@/types";
 import Button from "./form/Button";
@@ -9,47 +7,51 @@ import { MdContentCopy } from "react-icons/md";
 import { FcLike } from "react-icons/fc";
 import { calculateDiscountedPrice } from "@/functions";
 
-export default function CardAddToCart({ brandId, off, price, isLiked }: CardAddToCartProps) {
+export default function CardAddToCart({ brand, off, price, isLiked }: CardAddToCartProps) {
 	return (
 		<div
-			className='h-fit min-w-fit flex items-center flex-row-reverse lg:flex-col justify-between lg:justify-normal z-30 border-2 bg-neutral-100 rounded-lg shadow-inner lg:divide-y-2 divide-neutral-200 px-3 py-5 overflow-hidden
-		 '>
-			<div className='brand-wrapper flex-col gap-2 items-center h-40 w-full hidden lg:flex '>
+			className="h-fit flex items-center flex-row-reverse lg:flex-col justify-between lg:justify-normal z-30 border-2 bg-neutral-100 rounded-lg shadow-inner lg:divide-y-2 divide-neutral-200 px-3 py-5 overflow-hidden
+		 ">
+			<div className="brand-wrapper flex-col gap-2 items-center h-40 w-full hidden lg:flex ">
 				<Image
-					src={brand.src}
+					src={`http://localhost:4000/brandPic/${brand.brandPic}`}
 					width={384}
 					height={150}
-					alt='brand image'
-					className='h-full lg:w-full lg:h-40 object-cover rounded-md'
+					alt="brand image"
+					className="h-full lg:w-full lg:h-40 object-cover rounded-md"
 				/>
-				<span className='text-neutral-700 hover:text-neutral-900 text-xs'>
-					<Link href={"/"}>برای مشاهده همه محسولات برند یو</Link>
+				<span className="text-neutral-700 hover:text-neutral-900 text-xs">
+					<Link href={brand.enName === "unknown" ? "/brands" : `/brands/${brand.enName}`}>
+						برای مشاهده همه محسولات برند {brand.name}
+					</Link>
 				</span>
 			</div>
-			<div className='lg:w-full lg:mt-10 flex items-center justify-between sm:px-5 py-3'>
-				<span className='hidden lg:inline'>قیمت :</span>
-				<div className='flex flex-col sm:flex-row items-center gap-4'>
+			<div className="lg:w-full lg:mt-10 flex items-center justify-between sm:px-5 py-3">
+				<span className="hidden lg:inline">قیمت :</span>
+				<div className="flex flex-col sm:flex-row items-center gap-4">
 					{price > 0 && (
-						<span className='px-3 py-0 h-fit  rounded-full inline-block bg-secondary-500 text-white text-base font-semibold'>
+						<span className="px-3 py-0 h-fit  rounded-full inline-block bg-secondary-500 text-white text-base font-semibold">
 							{off}%
 						</span>
 					)}
-					<div className='flex flex-col gap-2'>
+					<div className="flex flex-col gap-2">
 						{price === 0 ? (
-							<span className='text-secondary-700 font-bold'>رایگان</span>
+							<span className="text-secondary-700 font-bold">رایگان</span>
 						) : (
 							<>
 								{price > 0 && !off && (
-									<span className='text-sm sm:text-base xl:text-lg font-bold'>{price}</span>
+									<span className="text-sm sm:text-base xl:text-lg font-bold">
+										{Math.floor(price)}
+									</span>
 								)}
 								{price > 0 && off && (
-									<span className='line-through text-sm sm:text-base xl:text-lg font-bold text-neutral-700'>
-										{price.toLocaleString()} تومان
+									<span className="line-through text-sm sm:text-base xl:text-lg font-bold text-neutral-700">
+										{Math.floor(price).toLocaleString()} تومان
 									</span>
 								)}
 								{off && off > 0 && (
-									<span className='text-sm sm:text-base xl:text-lg font-bold text-secondary-700'>
-										{calculateDiscountedPrice(price, off).toLocaleString()} تومان
+									<span className="text-sm sm:text-base xl:text-lg font-bold text-secondary-700">
+										{Math.floor(calculateDiscountedPrice(price, off)).toLocaleString()} تومان
 									</span>
 								)}
 							</>
@@ -58,22 +60,34 @@ export default function CardAddToCart({ brandId, off, price, isLiked }: CardAddT
 				</div>
 			</div>
 			<Button
-				colorScheme='primary'
-				typeBtn='text'
-				variant='fill'
-				type='button'
-				size='lg'
-				className='w-[40%!important] min-w-fit lg:w-[100%!important] h-[44px!important] text-sm sm:text-base lg:text-lg lg:mt-5 '>
+				colorScheme="primary"
+				typeBtn="text"
+				variant="fill"
+				type="button"
+				size="lg"
+				className="w-[40%!important] min-w-fit lg:w-[100%!important] h-[44px!important] text-sm sm:text-base lg:text-lg lg:mt-5 ">
 				افزودن به سبد
 			</Button>
-			<div className='hidden lg:flex items-center justify-center gap-9 mt-3 pt-2'>
-				<Button colorScheme='secondary' typeBtn='icon' variant='text'>
+			<div className="hidden lg:flex items-center justify-center gap-9 mt-3 pt-2">
+				<Button colorScheme="secondary" typeBtn="icon" variant="text">
 					{isLiked ? <FcLike /> : <FaRegHeart />}
 				</Button>
-				<Button colorScheme='secondary' typeBtn='icon' variant='text'>
-					<MdContentCopy />
-				</Button>
+				<ContentCopyBtn />
 			</div>
 		</div>
 	);
 }
+
+const ContentCopyBtn = () => {
+	"use client";
+	return (
+		<Button
+			colorScheme="secondary"
+			typeBtn="icon"
+			variant="text"
+			// onClick={() => navigator.clipboard.writeText(window.location.href)}
+		>
+			<MdContentCopy />
+		</Button>
+	);
+};
