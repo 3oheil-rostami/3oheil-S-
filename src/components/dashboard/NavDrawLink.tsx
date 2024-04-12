1dfbf3c66e09e7cdc0e6109ad3f1c30202fbf989
PwZakarya <hasanzadahzakarya@gmail.com>
2024-04-12 17:14:24 +0330

    added className property to NavDrawLink component

--- src/components/dashboard/NavDrawLink.tsx
+++ src/components/dashboard/NavDrawLink.tsx
@@ -1,12 +1,12 @@
 import { NavDrawLinkProps } from "@/types";
 import Link from "next/link";
 
-const NavDrawLink = ({ title, link, icon }: NavDrawLinkProps) => {
+const NavDrawLink = ({ title, link, icon, className = "" }: NavDrawLinkProps) => {
 	return (
-		<li className='border-2 border-neutral-200 rounded-md text-base'>
-			<Link href={link} className='flex items-center gap-2 text-neutral-800 hover:text-neutral-950'>
-				<span className='p-2 text-xl rounded-full'>{icon}</span>
-				<span className='mt-1'>{title}</span>
+		<li className={`border-2 border-neutral-200 rounded-md text-base list-none ${className}`}>
+			<Link href={link} className="flex items-center gap-2 text-neutral-800 hover:text-neutral-950">
+				<span className="p-2 text-xl rounded-full">{icon}</span>
+				<span className="mt-1">{title}</span>
 			</Link>
 		</li>
 	);
