e6a68099661b1cba141cec63b8ed65bcc5b1cc64
PwZakarya <hasanzadahzakarya@gmail.com>
2024-04-12 17:16:46 +0330

    used from HomeLayout and refactor based on HomeLayout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,13 @@
-import MainLayout from "./MainLayout";
-import Topbar from "@/components/Topbar";
-import Navbar from "@/components/Navbar";
-import NavbarMenu from "@/components/NavbarMenu";
 import HeaderSlider from "@/components/HeaderSlider";
 import ContainerSlider from "@/components/ContainerSlider";
 import CategoriesWithImages from "@/components/CategoriesWithImages";
 import Brands from "@/components/Brands";
-import Footer from "@/components/Footer";
 import LastArticles from "@/components/LastArticles";
+import HomeLayout from "./HomeLayout";
 
 export default function Home() {
-	
 	return (
-		<MainLayout>
-			<Topbar />
-			<div className='w-full flex flex-col items-center justify-center divide-y-2 divide-secondary-100'>
-				<Navbar />
-				<NavbarMenu />
-			</div>
+		<HomeLayout>
 			<HeaderSlider />
 			<div className='w-full flex flex-col items-center justify-center gap-y-4 my-4'>
 				<ContainerSlider type='promotion' />
@@ -26,7 +16,6 @@ export default function Home() {
 				<ContainerSlider type='top-sales' />
 				<LastArticles />
 			</div>
-			<Footer />
-		</MainLayout>
+		</HomeLayout>
 	);
 }
