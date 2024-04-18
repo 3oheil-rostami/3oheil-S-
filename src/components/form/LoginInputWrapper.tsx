e4b33ac920ea03748d5575c228eba3a4f9d99928
PwZakarya <hasanzadahzakarya@gmail.com>
2024-04-19 00:43:49 +0330

    edit style LoginInputWrapper.tsx component

--- src/components/form/LoginInputWrapper.tsx
+++ src/components/form/LoginInputWrapper.tsx
@@ -5,17 +5,21 @@ interface LoginInputProps {
 	icon: ReactNode;
 	children: ReactNode;
 	errorMessage?: string;
+	isDisabled?: boolean;
 }
 
-const LoginInputWrapper = ({ icon, errorMessage, children }: LoginInputProps) => {
+const LoginInputWrapper = ({ icon, errorMessage, isDisabled, children }: LoginInputProps) => {
 	return (
-		<div className='mb-4'>
-			<div className='flex items-center border-2 py-2 px-3 rounded-2xl *:last-of-type:bg-transparent *:text-neutral-800'>
+		<div className={`mb-4 ${isDisabled ? "opacity-40" : ""}`}>
+			<div
+				className={`flex items-center border-2 py-2 px-3 rounded-2xl *:last-of-type:bg-transparent *:text-neutral-800 ${
+					!!errorMessage ? "border-red-600" : ""
+				}`}>
 				{icon}
 				{children}
 			</div>
 			{!!errorMessage && (
-				<p className='mt-2 text-xs font-bold text-red-600 bg-red-100 py-1 px-2 rounded-md'>
+				<p className="mt-2 text-xs font-bold text-red-600 bg-red-100 py-1 px-2 rounded-md">
 					{errorMessage}
 				</p>
 			)}
