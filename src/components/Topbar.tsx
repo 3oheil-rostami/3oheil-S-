777320641afef0616501af35e5ebe8aa1849ea8d
PwZakarya <hasanzadahzakarya@gmail.com>
2024-04-19 00:48:50 +0330

    refactor and used from Image Component  for memoized

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { memo } from "react";
 import topbarImage from "@/../public/images/topbar.gif";
-export default function Topbar() {
+import Image from "next/image";
+const TopBar = (srcImage?: string) => {
 	return (
-		<div id="top" className='w-full bg-red-200'>
-			<img src={topbarImage.src} alt='topbarImage' />
+		<div id="top" className={`w-full bg-red-200 relative h-16 ${!srcImage ? "scale-y-0" : ""}`}>
+			<Image
+				fill
+				src={topbarImage.src}
+				alt="topbarImage"
+				style={{ inset: 0, objectFit: "cover", display: !srcImage ? "none" : "unset" }}
+			/>
 		</div>
 	);
-}
+};
+
+export default memo(TopBar);
