dc5cda28caf7f2fb5c33bfb2dd8bf31897a8e8f5
PwZakarya <hasanzadahzakarya@gmail.com>
2024-04-19 00:52:13 +0330

    added more services for products

--- src/services/product/index.ts
+++ src/services/product/index.ts
@@ -1,5 +1,13 @@
-import { httpService } from "../http-service";
+import { httpService, responseInterceptor } from "../http-service";
 
 const getProduct = (enName: string) => httpService.get(`/product/get/${enName}`);
 
-export { getProduct };
+const addToCart = (productId: string | number, colorId: string | number) =>
+	responseInterceptor.post(`/cart/new/${productId}`, { colorId });
+
+const removeToCart = (productId: string | number, colorId: string | number) =>
+	responseInterceptor.delete(`/cart/delete/${productId}`, { data: { colorId } });
+
+const getAllProductsInCart = () => responseInterceptor.get("/product/getAll");
+
+export { getProduct, addToCart, removeToCart, getAllProductsInCart };
