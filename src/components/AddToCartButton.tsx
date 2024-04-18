5bd3b079ff5f71f8e7ae192a967fdaaa438b91ff
PwZakarya <hasanzadahzakarya@gmail.com>
2024-04-19 00:45:03 +0330

    added increment and decrement logic and conceited to api

--- src/components/AddToCartButton.tsx
+++ src/components/AddToCartButton.tsx
@@ -1,25 +1,81 @@
 "use client";
+import { addToCart, removeToCart } from "@/services/product";
 import { AddToCartButtonProps } from "@/types";
+import { AxiosResponse } from "axios";
 import React, { useEffect, useState } from "react";
-import { MdDeleteOutline, MdOutlineAdd } from "react-icons/md";
+import { MdDelete, MdDeleteOutline, MdOutlineAdd } from "react-icons/md";
+import { toast } from "react-toastify";
+import Button from "./form/Button";
+import Link from "next/link";
+import { TiDelete } from "react-icons/ti";
 
 export default function AddToCartButton({
 	count = 0,
 	incrementAction,
 	decrementAction,
+	productId,
+	colorId,
 }: AddToCartButtonProps<HTMLButtonElement>) {
 	const [countNum, setCountNum] = useState<number>(0);
 	useEffect(() => {
 		setCountNum(count);
 	}, [count]);
 
-	const incrementHandler = () => {
+	const incrementHandler = async () => {
 		incrementAction && incrementAction();
 		setCountNum(prevValue => prevValue + 1);
+		const toastId = toast.loading("در حال اضافه کردن به سبد خرید ...");
+		const response = await addToCart(productId, colorId);
+		toast.update(toastId, {
+			isLoading: false,
+			autoClose: 3000,
+			type: response.status === 201 ? "success" : "error",
+			render() {
+				response.status !== 201 && setCountNum(prevValue => prevValue - 1);
+				return response.status === 401 ? (
+					<div className="flex justify-between items-center gap-2">
+						<p>ابتدا لاگین کنید .</p>
+						<Button colorScheme="secondary" variant="outline" size="2xs">
+							<Link href={"/login"}>لاگین</Link>
+						</Button>
+					</div>
+				) : response.status === 422 ? (
+					<p>تعداد موجود کافی نیست .</p>
+				) : response.status === 201 ? (
+					<p>با موفقیت اضافه شد .</p>
+				) : response.status === 500 ? (
+					<p>سرور با مشکل مواجه شد</p>
+				) : (
+					<p>خطای ناشناس</p>
+				);
+			},
+		});
 	};
-	const decrementHandler = () => {
-		decrementAction && decrementAction();
-		setCountNum(prevValue => prevValue - 1);
+	const decrementHandler = async () => {
+		if (navigator.onLine) {
+			decrementAction && decrementAction();
+			const toastId = toast.loading("در حال حذف محصول از سبد خرید ...");
+			const response = await removeToCart(productId, colorId);
+			toast.update(toastId, {
+				isLoading: false,
+				autoClose: 3000,
+				type: response.status === 200 ? "success" : "error",
+				icon:
+					response.status === 200 ? <MdDelete className="text-xl text-green-600" /> : <TiDelete />,
+				render() {
+					response.status === 200 && setCountNum(prevValue => prevValue - 1);
+					return response.status === 200
+						? "از سبد حذف شد ."
+						: response.status === 404
+						? "همچین محصولی داخل سبد خریدتون موجود نیست ."
+						: response.status === 500
+						? "خطا سمت سرور رخ داد ."
+						: "خطای ناشناس";
+				},
+			});
+		} else {
+			toast.warning("شما آنلاین نیستید .");
+		}
 	};
 
 	return (
