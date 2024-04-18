4a0247c03028784a10b83161312bd4ad74db114b
PwZakarya <hasanzadahzakarya@gmail.com>
2024-04-19 00:57:55 +0330

    provided MainLayout.tsx

--- src/app/MainLayout.tsx
+++ src/app/MainLayout.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import RootLayout from "./layout";
+import { Flip, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import ContextProvider from "@/context/Context";
 
 export default function MainLayout({
 	children,
@@ -7,8 +10,18 @@ export default function MainLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<RootLayout>
-			<div className='main-layout'>{children}</div>
-		</RootLayout>
+		<ContextProvider>
+			<RootLayout>
+				<div className="main-layout">{children}</div>
+				<ToastContainer
+					position="bottom-center"
+					rtl
+					autoClose={3000}
+					draggable
+					transition={Flip}
+					className={"font-bold text-base text-neutral-800"}
+				/>
+			</RootLayout>
+		</ContextProvider>
 	);
 }
