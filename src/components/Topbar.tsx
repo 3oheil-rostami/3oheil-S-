502fcca1bde7ec6556c0d98d87cf25343ee7a314
PwZakarya <hasanzadahzakarya@gmail.com>
2024-05-14 04:02:59 +0330

    add alert text to topbar

--- src/components/Topbar.tsx
+++ src/components/Topbar.tsx
@@ -2,15 +2,19 @@ import React, { memo } from "react";
 import Image from "next/image";
 const TopBar = ({ srcImage }: { srcImage?: string }) => {
 	return (
-		<div id="top" className={`w-full relative ${!srcImage ? "h-0" : "h-16"}`}>
-			{!!srcImage && (
+		<div id="top" className={`w-full relative ${srcImage ? "h-0" : "h-16"} bg-primary-200`}>
+			{/* {!!srcImage && (
 				<Image
 					fill
 					src={srcImage}
 					alt="topbarImage"
 					style={{ inset: 0, objectFit: "cover", display: !srcImage ? "none" : "unset" }}
 				/>
-			)}
+			)} */}
+			<h1 className="text-white font-bold text-3xl text-center">
+				{" "}
+				وبسایت در حال توسعه است ، به همین خاطر ایرادات زیادی میبینید.(البته فعلا )
+			</h1>
 		</div>
 	);
 };
